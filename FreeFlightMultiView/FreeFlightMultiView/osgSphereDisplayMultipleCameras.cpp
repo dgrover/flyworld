bb300566a8c4ccb42dd3c64bd2bbcb227ac87086
jfshiuan <jfshiuan@ucsd.edu>
2014-08-04 11:27:39 -0700

    Free-Flight Display

--- FreeFlightMultiView/FreeFlightMultiView/osgSphereDisplayMultipleCameras.cpp
+++ FreeFlightMultiView/FreeFlightMultiView/osgSphereDisplayMultipleCameras.cpp
@@ -4,64 +4,27 @@
 #include <osg/ShapeDrawable>
 #include <osg/Texture2D>
 
-double imageWidth=1366;//800;
-double imageHeight=768;//1580;
+double screenWidth=1366;//800;
+double screenHeight=768;//1580;
+
+	int width=screenWidth/3;//800;
+	int height=screenWidth/3;//1580;
+
  int xoffset = 0;//;1920;
-    int yoffset = 0;//-150;
-	int width=imageWidth/4;//800;
-	int height=imageHeight;//1580;
+    int yoffset = (screenHeight-height)/2;//-150;
 
-
-double fovy=30;
-
-double boxHorMin=0;
-double boxVertMin=0;
-double boxHorMax=boxHorMin + imageWidth;
-double boxVertMax=boxVertMin + imageHeight;
 
 osg::Vec4 backgroundColor = osg::Vec4(0, 0, 0, 1);
 
 double diam=3.14159*2;
-double numBoxes=1;
 double depth=0;
-
-double rS= 0.1*diam; //rotation speed
-//double camHorLoc=boxHorMin+imageWidth/2*numBoxes; //location of camera
+double rS= 0.01*diam; //rotation speed
 double camHorLoc=0;
 double camVertLoc=0;
-double distance=13.5;
+double distance=19.25;
 osg::Vec3d up=osg::Vec3d(0,0,1); //up vector
 
 std::string imageFileName="numberline.gif";//"vert_stripe.bmp";
-
-class GeodeUpdateCallback : public osg::NodeCallback 
-{ 
-public : 
-
-void operator()( osg::Node * node , osg::NodeVisitor * nv) 
-{ 
-	double currTime;
-if (nv->getFrameStamp())
-{
-			 currTime= nv->getFrameStamp()->getSimulationTime();
-}
-osg::Vec3f* rotationVec=new osg::Vec3(0,0,1);
-osg::Quat* quat = new osg::Quat(rS*currTime, *rotationVec);
-
-
-osg::Geode * geode = dynamic_cast< osg::Geode * >( node ); 
-osg::Drawable* drawable = geode->getDrawable(0);
-//osg::ShapeDrawable * geom = dynamic_cast< osg::ShapeDrawable * >( node ) ; 
-
-osg::Shape* cyl = ((osg::ShapeDrawable*) drawable)->getShape();
-((osg::Cylinder*)cyl)->setRotation(*quat);
-((osg::ShapeDrawable*) drawable)->setShape(cyl);
-
-
-} 
-}; 
-
-
 
 
 
@@ -97,12 +60,14 @@ class TextureUpdateCallback : public osg::NodeCallback
 
 
 
+
+
+
 osg::Geode* createShapes()
 {
     osg::Geode* geode = new osg::Geode();
-
     osg::StateSet* stateset = new osg::StateSet();
-	stateset->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
+	stateset->setMode(GL_LIGHTING, osg::StateAttribute::ON);
 	osg::Image* image = new osg::Image();
 	 image = osgDB::readImageFile(imageFileName);
 	
@@ -117,13 +82,12 @@ osg::Geode* createShapes()
     }
     
     geode->setStateSet( stateset );
-	geode->setCullingActive(false);
+	geode->setCullingActive(true);
     
-    float radius = 3;//5; //0.8f;
-    float height = 3.8;//3.6; //5.02654824574*8/12;
+    float radius = 4.8;//5; //0.8f;
     
    osg::TessellationHints* hints = new osg::TessellationHints;
-    hints->setDetailRatio(0.8f);
+    hints->setDetailRatio(3.0f);
 
 
    hints->setCreateBackFace(true);
@@ -131,36 +95,37 @@ hints->setCreateFrontFace(false);
 hints->setCreateNormals(false);
 hints->setCreateTop(false);
 hints->setCreateBottom(false);
-osg::Vec3f* rotationVec=new osg::Vec3(0,0,1);
-osg::Quat* quat = new osg::Quat(30, *rotationVec);
-osg::Cylinder* cyl = new osg::Cylinder(osg::Vec3(0.0f,0.0f,0.0f),radius,height);
-cyl->setRotation(*quat);
-osg::ShapeDrawable* cylinder = new osg::ShapeDrawable(cyl ,hints);
-  cylinder->setUseDisplayList(false);
-    geode->addDrawable(cylinder);
-   
-    geode->setUpdateCallback(new GeodeUpdateCallback());
+osg::Sphere* sph = new osg::Sphere(osg::Vec3(0.0f,0.0f,0.0f),radius);
 
-	//osg::TexMat* texmat = (osg::TexMat*)(stateset->getTextureAttribute(0,osg::StateAttribute::TEXMAT));	
-	//geode->setUpdateCallback(new TextureUpdateCallback(texmat));
+osg::ShapeDrawable* sphere = new osg::ShapeDrawable(sph ,hints);
+  sphere->setUseDisplayList(false);
+
+    geode->addDrawable(sphere);
+   
+	osg::TexMat* texmat = (osg::TexMat*)(stateset->getTextureAttribute(0,osg::StateAttribute::TEXMAT));	
+	geode->setUpdateCallback(new TextureUpdateCallback(texmat));
     return geode;
 }
 
 int main( int argc, char **argv )
 {
+// construct the viewer.
 	osgViewer::Viewer viewer;
 
 
+ // create the model
     osg::Group* root = new osg::Group;
     root->addChild( createShapes() );
 
 
     viewer.setSceneData( root );
+//osg::ref_ptr<osg::Camera> myCam = new osg::Camera;
+//myCam->setClearColor(backgroundColor); // black background
+	// set dimensions of the view volume
+	//myCam->setProjectionMatrixAsPerspective(fovy, 3.0 / 3.0, 0.1, 3000);
+	//viewer.setCamera(myCam); // attach camera to the viewer
+   
 
-
-
-
-  /////
 
     // front
     {
@@ -181,13 +146,6 @@ int main( int argc, char **argv )
         GLenum buffer = traits->doubleBuffer ? GL_BACK : GL_FRONT;
         camera->setDrawBuffer(buffer);
         camera->setReadBuffer(buffer);
-		//camera->setClearColor(backgroundColor); // black background
-		//camera->setViewMatrixAsLookAt(osg::Vec3d(camHorLoc,distance,camVertLoc), osg::Vec3d(camHorLoc,depth,camVertLoc), up);
-	// set dimensions of the view volume
-	//camera->setProjectionMatrixAsPerspective(fovy, 3.0 / 3.0, 0.1, 3000);
-
-        // add this slave camera to the viewer, with a shift left of the projection matrix
-       // viewer.addSlave(camera.get(), osg::Matrixd::translate(1.0,0.0,0.0), osg::Matrixd());
 		viewer.addSlave(camera.get(), osg::Matrixd(), osg::Matrixd());
     }
     
@@ -210,63 +168,23 @@ int main( int argc, char **argv )
         GLenum buffer = traits->doubleBuffer ? GL_BACK : GL_FRONT;
         camera->setDrawBuffer(buffer);
         camera->setReadBuffer(buffer);
-			//camera->setClearColor(backgroundColor); // black background
-	// set dimensions of the view volume
-//	camera->setProjectionMatrixAsPerspective(fovy, 3.0 / 3.0, 0.1, 3000);
-//camera->setViewMatrixAsLookAt(osg::Vec3d(camHorLoc,distance,camVertLoc), osg::Vec3d(camHorLoc,depth,camVertLoc), up);
-        // add this slave camera to the viewer, with a shift right of the projection matrix
-      //  viewer.addSlave(camera.get(), osg::Matrixd::translate(-1.0,0.0,0.0), osg::Matrixd()); //original
 
-osg::Matrixd camRot;
-osg::Matrixd camTrans;
-camRot.makeRotate(osg::DegreesToRadians(180.0), osg::Vec3(0,1,0));
-camTrans.makeTranslate(0.0,0.0,distance*2);
-
-//viewer.addSlave(camera.get(), osg::Matrixd(), osg::Matrixd::translate(0.0,0.0,distance*2));
-	viewer.addSlave(camera.get(), osg::Matrixd(),camTrans*camRot);
-	//viewer.addSlave(camera.get(), osg::Matrixd(),osg::Matrixd::rotate(osg::DegreesToRadians(180.0), osg::Vec3(0,1,0)).translate(0.0,0.0,distance*2));
-    }
-
-
-//left side
-	
- {
-        osg::ref_ptr<osg::GraphicsContext::Traits> traits = new osg::GraphicsContext::Traits;
-        traits->x = xoffset+width*3;
-        traits->y = yoffset;
-        traits->width = width;
-        traits->height = height;
-        traits->windowDecoration = false;
-        traits->doubleBuffer = true;
-        traits->sharedContext = 0;
-
-        osg::ref_ptr<osg::GraphicsContext> gc = osg::GraphicsContext::createGraphicsContext(traits.get());
-
-        osg::ref_ptr<osg::Camera> camera = new osg::Camera;
-        camera->setGraphicsContext(gc.get());
-        camera->setViewport(new osg::Viewport(0,0, traits->width, traits->height));
-        GLenum buffer = traits->doubleBuffer ? GL_BACK : GL_FRONT;
-        camera->setDrawBuffer(buffer);
-        camera->setReadBuffer(buffer);
-			//camera->setClearColor(backgroundColor); // black background
-	// set dimensions of the view volume
-//	camera->setProjectionMatrixAsPerspective(fovy, 3.0 / 3.0, 0.1, 3000);
-//camera->setViewMatrixAsLookAt(osg::Vec3d(camHorLoc,distance,camVertLoc), osg::Vec3d(camHorLoc,depth,camVertLoc), up);
-        // add this slave camera to the viewer, with a shift right of the projection matrix
-      //  viewer.addSlave(camera.get(), osg::Matrixd::translate(-1.0,0.0,0.0), osg::Matrixd()); //original
 
 osg::Matrixd camRot;
 osg::Matrixd camTrans1;
 osg::Matrixd camTrans2;
+
 camTrans1.makeTranslate(0.0,0.0,distance);
-camRot.makeRotate(osg::DegreesToRadians(90.0), osg::Vec3(0,1,0));
+camRot.makeRotate(osg::DegreesToRadians(120.0), osg::Vec3(0,1,0));
 camTrans2.makeTranslate(0.0,0.0,-1*distance);
+
+
+
 
 //viewer.addSlave(camera.get(), osg::Matrixd(), osg::Matrixd::translate(0.0,0.0,distance*2));
 	viewer.addSlave(camera.get(), osg::Matrixd(),camTrans1*camRot*camTrans2);
 	//viewer.addSlave(camera.get(), osg::Matrixd(),osg::Matrixd::rotate(osg::DegreesToRadians(180.0), osg::Vec3(0,1,0)).translate(0.0,0.0,distance*2));
     }
-
 
 
 
@@ -294,18 +212,12 @@ camTrans2.makeTranslate(0.0,0.0,-1*distance);
         GLenum buffer = traits->doubleBuffer ? GL_BACK : GL_FRONT;
         camera->setDrawBuffer(buffer);
         camera->setReadBuffer(buffer);
-			//camera->setClearColor(backgroundColor); // black background
-	// set dimensions of the view volume
-//	camera->setProjectionMatrixAsPerspective(fovy, 3.0 / 3.0, 0.1, 3000);
-//camera->setViewMatrixAsLookAt(osg::Vec3d(camHorLoc,distance,camVertLoc), osg::Vec3d(camHorLoc,depth,camVertLoc), up);
-        // add this slave camera to the viewer, with a shift right of the projection matrix
-      //  viewer.addSlave(camera.get(), osg::Matrixd::translate(-1.0,0.0,0.0), osg::Matrixd()); //original
 
 osg::Matrixd camRot;
 osg::Matrixd camTrans1;
 osg::Matrixd camTrans2;
 camTrans1.makeTranslate(0.0,0.0,distance);
-camRot.makeRotate(osg::DegreesToRadians(-90.0), osg::Vec3(0,1,0));
+camRot.makeRotate(osg::DegreesToRadians(-120.0), osg::Vec3(0,1,0));
 camTrans2.makeTranslate(0.0,0.0,-1*distance);
 
 //viewer.addSlave(camera.get(), osg::Matrixd(), osg::Matrixd::translate(0.0,0.0,distance*2));
